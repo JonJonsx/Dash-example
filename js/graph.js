51fb245762baf8d524a9ad9b4cddf7b22bcbef1a
JonJonsx <jonatan.souza@bandtec.com.br>
2020-06-04 02:03:20 -0300

    Grafico sendo reajustado para caber no elemento

--- js/graph.js
+++ js/graph.js
@@ -17,6 +17,8 @@ var population = new Chart(graphicPopulation, {
     options: {}
 });
 
+const labels = ['1','2','3','4','5','6','7','8'];
+
 var graphUtility = document.getElementById('graph-utility').getContext('2d');
 var utility = new Chart(graphUtility, {
     // The type of chart we want to create
@@ -24,10 +26,10 @@ var utility = new Chart(graphUtility, {
 
     // The data for our dataset
     data: {
-        labels: ['1','2','3','4','5','6','7','8'],
+        labels,
         datasets: [{
             label: 'Vaga mais ocupada',
-            backgroundColor: getRandomColor(),
+            backgroundColor: labels.map(label => getRandomColor()),
             borderColor: ['#e74c3c', '#2ecc71'],
             data: [30, 1, 15, 64, 8, 9, 32, 40]
         }]
