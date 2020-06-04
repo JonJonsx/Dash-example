b1451050c6c1bac23402cd5597f1378383d268fb
JonJonsx <jonatan.souza@bandtec.com.br>
2020-06-03 22:14:36 -0300

    grafico estatico de ocupaçao adicionado e transição de menu adicionada

---
+++ js/graph.js
@@ -0,0 +1,47 @@
+var graphicPopulation = document.getElementById('graph-Population').getContext('2d');
+var population = new Chart(graphicPopulation, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['Ocupado', 'Vazio'],
+        datasets: [{
+            backgroundColor: ['#e74c3c', '#2ecc71'],
+            borderColor: ['#e74c3c', '#2ecc71'],
+            data: [30, 70]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+var graphUtility = document.getElementById('graph-utility').getContext('2d');
+var utility = new Chart(graphUtility, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['1','2','3','4','5','6','7','8'],
+        datasets: [{
+            label: 'Vaga mais ocupada',
+            backgroundColor: getRandomColor(),
+            borderColor: ['#e74c3c', '#2ecc71'],
+            data: [30, 1, 15, 64, 8, 9, 32, 40]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
---
+++ js/menu.js
@@ -0,0 +1,15 @@
+function closeOpenBar(){
+    var closeOpen = document.getElementById('closeOpen');
+    var sideBar = document.getElementById('sideBar');
+    var dashBoard = document.getElementById('main-Dashboard');
+    debugger;
+    if(closeOpen.innerHTML == 'x'){
+        sideBar.style.width = '40px';
+        dashBoard.style.marginLeft = '40px';
+        closeOpen.innerHTML = '☰';
+    }else if(closeOpen.innerHTML == '☰'){
+        sideBar.style.width = '200px';
+        dashBoard.style.marginLeft = '200px';
+        closeOpen.innerHTML = 'x';
+    }
+}
